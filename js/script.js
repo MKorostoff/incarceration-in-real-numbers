9a47be252c63802d7f36af063254ae5ba28bdd75
Matt Korostoff <MKorostoff@gmail.com>
2020-07-01 20:29:34 -0400

    Evening commit

--- js/script.js
+++ js/script.js
@@ -39,3 +39,11 @@ function getScrollCount() {
   var count = Math.floor(scroll_percent * 2300000);
   return count;
 }
+
+function toggleExpand(outer, inner) {
+  var outerEl = document.getElementById(outer);
+  var innerEl = document.getElementById(inner);
+  var offset = Math.abs(outerEl.offsetTop - innerEl.offsetTop);
+  innerEl.style.top = offset + 'px';
+  outerEl.classList.add('expanded')
+}

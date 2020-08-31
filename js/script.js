30538a572c92fcf68f5c65701ac821c28c3b0ecc
Matt Korostoff <MKorostoff@gmail.com>
2020-08-31 10:33:48 -0400

    Intermediate commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var prisoners = document.getElementById('prisoners');
+var thousand = document.getElementById('thousand');
 var counter = document.getElementById('counter');
 var title = document.getElementById('title');
 var scroll_count = 0;
@@ -6,43 +7,49 @@ var scroll_count = 0;
 function setHeight() {
   var browser_width = window.innerWidth || document.body.clientWidth;
   var icons_per_card = 200;
-  var cards_per_row = browser_width / 400;
+  var pixel_height_per_card = 500;
+  var pixel_width_per_card = 400;
+
+  var cards_per_row = browser_width / pixel_width_per_card;
   var icons_per_row = icons_per_card * cards_per_row;
   var number_of_rows = 2300000/icons_per_row;
-  var height = Math.floor(number_of_rows * 500);
+
+  var height = Math.floor(number_of_rows * pixel_height_per_card);
   prisoners.style.height = height + "px";
+
+  var thousand_height = Math.floor((1000/icons_per_row) * pixel_height_per_card);
+  thousand.style.height = thousand_height + "px";
 }
 setHeight();
 
 window.addEventListener("orientationchange", setHeight);
 window.addEventListener("resize", setHeight);
 
-
 var observer = new IntersectionObserver(function(entries){
   entries.forEach(function(entry){
     if (entry.isIntersecting || entry.intersectionRatio > 0) {
-      prisoners.style.backgroundImage = "url('img/bg/" + entry.target.dataset.background + "')"
+      prisoners.classList.remove('red');
+      prisoners.classList.remove('blue');
+      prisoners.classList.remove('black');
+      prisoners.classList.add(entry.target.dataset.background);
+      document.getElementsByTagName('body')[0].classList.remove('not-free');
     }
   })
 })
-var obervables = ['blue','red','pretrial','preconvicted', 'unsurprisingly', 'unconvicted'];
-obervables.forEach(function(id){
-  var target = document.getElementById(id);
+document.querySelectorAll('[data-background]').forEach(function(target){
   observer.observe(target);
-})
+});
 
 var freedomObserver = new IntersectionObserver(function(entries){
   entries.forEach(function(entry){
     if (entry.isIntersecting || entry.intersectionRatio > 0) {
       document.getElementsByTagName('body')[0].classList.add('not-free');
     }
-    else {
-      document.getElementsByTagName('body')[0].classList.remove('not-free');
-    }
   })
 })
-var freedom = document.getElementById('freedom');
-freedomObserver.observe(freedom);
+document.querySelectorAll('.freedom').forEach(function(target){
+  freedomObserver.observe(target);
+});
 
 var ua = navigator.userAgent.toLowerCase();
 var isAndroid = ua.indexOf("android") > -1;
@@ -74,4 +81,12 @@ function toggleExpand(outer, inner) {
   var offset = Math.abs(outerEl.offsetTop - innerEl.offsetTop);
   innerEl.style.top = offset + 'px';
   outerEl.classList.add('expanded')
+}
+
+function showTooltip(e) {
+  var tooltip = e.parentElement.getElementsByClassName('tooltip')[0]
+  tooltip.classList.add('open')
+}
+function closeTooltip(e) {
+  e.parentElement.classList.remove('open');
 }
